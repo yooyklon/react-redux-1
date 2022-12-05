163375d56d1aa914298c679a361d65d7362465b0
Dmitry Sheshukov <yooyklon@gmail.com>
2022-12-05 17:25:43 +0700

    fix

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,52 +1,14 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 
 import WidgetList from "./WidgetList";
 
 import WidgetForm from "./WidgetForm";
 
 export default function Widget() {
-  const [form, setForm] = useState({
-    date: "",
-    passed: "",
-  });
-
-  const currentError = useRef();
-
-  const [list, setList] = useState([]);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    setList((prevList) => [...prevList, form]);
-
-    setForm({
-      date: "",
-      passed: "",
-    });
-  }
-
-  function inputChange(event) {
-    const name = event.target.name;
-
-    setForm((prevForm) => ({ ...prevForm, [name]: event.target.value }));
-  }
-
-  function removeListItem(date) {
-    setList((prevList) => [...prevList.filter((obj) => obj.date !== date)]);
-  }
-
-  function handleEditItem(date, passed) {
-    setForm({ date, passed });
-  }
-
   return (
     <div className="workout">
       <WidgetForm />
-      <WidgetList
-        list={list}
-        onEditItem={handleEditItem}
-        onRemoveItem={removeListItem}
-      />
+      <WidgetList />
     </div>
   );
 }
